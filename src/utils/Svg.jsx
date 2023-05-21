e12e10625b9714c0cb05eef7c7830ae5f13e5861
Sayan Batabyal <sayanbatabyal.mav@gmail.com>
2023-05-21 13:14:10 +0530

    navbar-sidebar

--- src/utils/Svg.jsx
+++ src/utils/Svg.jsx
@@ -3,7 +3,7 @@ export const GoogleLogo = ({className='',props}) => (
   <svg xmlns="http://www.w3.org/2000/svg"
  width={48} height={48} 
  viewBox="0 0 48 48"
- className={`${className} w-full h-auto`} {...props}>
+ className={`${className} w-4 h-4`} {...props}>
     <path
       fill="#FFC107"
       d="M43.611 20.083H42V20H24v8h11.303c-1.649 4.657-6.08 8-11.303 8-6.627 0-12-5.373-12-12s5.373-12 12-12c3.059 0 5.842 1.154 7.961 3.039l5.657-5.657C34.046 6.053 29.268 4 24 4 12.955 4 4 12.955 4 24s8.955 20 20 20 20-8.955 20-20c0-1.341-.138-2.65-.389-3.917z"
@@ -29,7 +29,7 @@ export const AppleLogo = ({className='',props}) => (
       width={48}
       height={48}
       viewBox="0 0 496.255 608.728"
-      className={`${className} w-full h-auto`}
+      className={`${className} w-4 h-4`}
       {...props}
     >
       <path
