e75795aac7da4bb2c926146f621ea6c55869e6af
Sayan Batabyal <sayanbatabyal.mav@gmail.com>
2023-05-22 13:34:40 +0530

    skeleton-added-2

--- src/components/Meet.jsx
+++ src/components/Meet.jsx
@@ -5,7 +5,7 @@ const Meet = ({load, title="Sayan", time="12.00-13.00" , location="at Durgapur",
   return (
     <div className='flex gap-1 '>
     {(load)? <Skeleton variant="rectangle" width={6} height={70} />:null} 
-    <div className={`w-[95%] flex flex-col gap-1 px-3 py-1 border-l-${load?0:4}`} style={{borderColor:`${border}`}}>
+    <div className={`w-[95%] flex flex-col gap-1 px-3 py-1 border-l-4`} style={{borderColor:`${border}`}}>
         
         {(load)? <Skeleton variant="text" width={'50%'} height={20} /> :<p className='text-sm font-bold text-[#666666]'>{title}</p>}
         {(load)? <Skeleton variant="text" width={'20%'} height={15} /> :<p className='text-xs text-[#999999]'>{time}</p>}
